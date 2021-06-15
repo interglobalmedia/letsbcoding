078b47db00835b8c90dc967f8d8160257b5f8644
Maria Campbell <interglobalmedia@gmail.com>
2021-06-15 15:07:29 -0400

    Add note post type

--- wp-content/mu-plugins/bcoding-post-types.php
+++ wp-content/mu-plugins/bcoding-post-types.php
@@ -86,6 +86,25 @@ function bcoding_post_types() {
         ),
         'menu_icon' => 'dashicons-awards'
     ));
+
+    // note post type
+    register_post_type('note', array(
+        'show_in_rest' => true,
+        'supports' => array(
+            'title', 
+            'editor'
+        ),
+        'public' => false,
+        'show_ui' => true,
+        'labels' => array(
+            'name' => 'Notes',
+            'add_new_item' => 'Add New Note',
+            'edit_item' => 'Edit Note',
+            'all_items' => 'All Notes',
+            'singular_name' => 'Note'
+        ),
+        'menu_icon' => 'dashicons-welcome-write-blog'
+    ));
 }
 
 add_action('init', 'bcoding_post_types');
