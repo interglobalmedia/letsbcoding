cbae863c10d2975397e1c3f31f1b1fdbca109b2b
Maria Campbell <interglobalmedia@gmail.com>
2021-06-24 08:59:53 -0400

    Add dynamic data-like to like-box

--- wp-content/themes/letsbcoding-theme/single-professor.php
+++ wp-content/themes/letsbcoding-theme/single-professor.php
@@ -28,6 +28,7 @@ while(have_posts()) {
               ));
 
               $existStatus = 'no';
+              
               if (is_user_logged_in()) {
                 $existQuery = new WP_Query(array(
                   'author' => get_current_user_id(),
@@ -47,7 +48,7 @@ while(have_posts()) {
 
               }
             ?>
-                <span class="like-box" data-professor="<?php the_ID(); ?>" data-exists="<?php echo $existStatus; ?>">
+                <span class="like-box" data-like="<?php echo $existQuery->posts[0]->ID; ?>" data-professor="<?php the_ID(); ?>" data-exists="<?php echo $existStatus; ?>">
                     <i class="fa fa-heart-o" aria-hidden="true"></i>
                     <i class="fa fa-heart" aria-hidden="true"></i>
                     <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
