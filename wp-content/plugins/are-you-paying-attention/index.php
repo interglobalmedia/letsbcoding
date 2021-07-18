d1b877234cfdfd289b1c3a3c9af2d6335c46f4ed
Maria Campbell <interglobalmedia@gmail.com>
2021-07-17 20:10:08 -0400

    Add build/index.js

--- wp-content/plugins/are-you-paying-attention/index.php
+++ wp-content/plugins/are-you-paying-attention/index.php
@@ -19,7 +19,7 @@ class AreYouPayingAttention {
     }
 
     function adminAssets() {
-        wp_enqueue_script('apaNewBlockType', plugin_dir_url(__FILE__) . 'test.js', array(   
+        wp_enqueue_script('apaNewBlockType', plugin_dir_url(__FILE__) . 'build/index.js', array(   
             'wp-blocks',
             'wp-element'
         ));
