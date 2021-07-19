31dc420d022f8827f36423f0ebdd54ff2804edb0
Maria Campbell <interglobalmedia@gmail.com>
2021-07-19 12:04:22 -0400

    Import more componeents scss and EditComponent function

--- wp-content/plugins/are-you-paying-attention/src/index.js
+++ wp-content/plugins/are-you-paying-attention/src/index.js
@@ -1,3 +1,6 @@
+import './index.scss'
+import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon } from '@wordpress/components'
+
 wp.blocks.registerBlockType('aypaplugin/are-you-paying-attention', {
     title: 'Are You Paying Attention?',
     icon: 'smiley',
@@ -10,25 +13,40 @@ wp.blocks.registerBlockType('aypaplugin/are-you-paying-attention', {
             type: 'string'
         }
     },
-    edit: function (props) {
-        function updateSkyColor(e) {
-            props.setAttributes({
-                skyColor: e.target.value
-            })
-        }
-        function updateGrassColor(e) {
-            props.setAttributes({
-                grassColor: e.target.value
-            })
-        }
-        return (
-            <div>
-                <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
-                <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
-            </div>
-        )
-    },
+    edit: EditComponent,
     save: function(props) {
         return null
     }
 })
+
+function EditComponent (props) {
+    function updateSkyColor(e) {
+        props.setAttributes({
+            skyColor: e.target.value
+        })
+    }
+    function updateGrassColor(e) {
+        props.setAttributes({
+            grassColor: e.target.value
+        })
+    }
+    return (
+        <div className="paying-attention-edit-block">
+            <TextControl label="Question:" />
+            <p>Answers:</p>
+            <Flex>
+                <FlexBlock>
+                    <TextControl />
+                </FlexBlock>
+                <FlexItem>
+                    <Button>
+                        <Icon icon="star-empty"/>
+                    </Button>
+                </FlexItem>
+                <FlexItem>
+                    <Button>Delete</Button>
+                </FlexItem>
+            </Flex>
+        </div>
+    )
+}
