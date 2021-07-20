3511b1037eda4d1c5eec02806067229149fa41fa
Maria Campbell <interglobalmedia@gmail.com>
2021-07-19 21:27:06 -0400

    Update attrs funcitons and JSX for dynamic multiple choice

--- wp-content/plugins/are-you-paying-attention/src/index.js
+++ wp-content/plugins/are-you-paying-attention/src/index.js
@@ -6,11 +6,12 @@ wp.blocks.registerBlockType('aypaplugin/are-you-paying-attention', {
     icon: 'smiley',
     category: 'common',
     attributes: {
-        skyColor: {
+        question: {
             type: 'string'
         },
-        grassColor: {
-            type: 'string'
+        answers: {
+            type: 'array',
+            default: ['red', 'blue']
         }
     },
     edit: EditComponent,
@@ -19,35 +20,45 @@ wp.blocks.registerBlockType('aypaplugin/are-you-paying-attention', {
     }
 })
 
-function EditComponent (props) {
-    function updateSkyColor(e) {
-        props.setAttributes({
-            skyColor: e.target.value
-        })
+function EditComponent(props) {
+    
+    function updateQuestion(value) {
+        props.setAttributes({question: value})
     }
-    function updateGrassColor(e) {
-        props.setAttributes({
-            grassColor: e.target.value
+    function deleteAnswer(indexToDelete) {
+        const newAnswers = props.attributes.answers.filter(function (x, index) {
+            return index !== indexToDelete
         })
+        props.setAttributes({answers: newAnswers})
     }
     return (
         <div className="paying-attention-edit-block">
-            <TextControl label="Question:" style={{fontSize: '1.25rem'}} />
+            <TextControl label="Question:" value={props.attributes.question} onChange={updateQuestion} style={{fontSize: '1.25rem'}} />
             <p style={{fontSize: '13px', margin: '20px 0 8px 0'}}>Answers:</p>
-            <Flex>
-                <FlexBlock>
-                    <TextControl/>
-                </FlexBlock>
-                <FlexItem>
-                    <Button>
-                        <Icon className="mark-as-correct" icon="star-empty"/>
-                    </Button>
-                </FlexItem>
-                <FlexItem>
-                    <Button isLink className="attention-delete">Delete</Button>
-                </FlexItem>
-            </Flex>
-            <Button isPrimary>Add another answer</Button>
+            {props.attributes.answers.map((answer, index) => {
+                return (
+                    <Flex>
+                        <FlexBlock>
+                            <TextControl value={answer} onChange={newValue => {
+                                const newAnswers = props.attributes.answers.concat([])
+                                newAnswers[index] = newValue
+                                props.setAttributes({answers: newAnswers})
+                            }} />
+                        </FlexBlock>
+                        <FlexItem>
+                            <Button>
+                                <Icon className="mark-as-correct" icon="star-empty"/>
+                            </Button>
+                        </FlexItem>
+                        <FlexItem>
+                            <Button isLink className="attention-delete" onClick={() => deleteAnswer(index)}>Delete</Button>
+                        </FlexItem>
+                    </Flex>
+                )
+            })}
+            <Button isPrimary onClick={() => {
+                props.setAttributes({answers: props.attributes.answers.concat([''])})
+            }}>Add another answer</Button>
         </div>
     )
 }
