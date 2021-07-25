1e07f218c6ba023032feb92837d879ded259d879
Maria Campbell <interglobalmedia@gmail.com>
2021-07-24 21:07:29 -0400

    Add translations capability

--- wp-content/plugins/featured-professor/featured-professor.php
+++ wp-content/plugins/featured-professor/featured-professor.php
@@ -5,6 +5,8 @@
   Version: 1.0
   Author: Maria D. Campbell
   Author URI: https://letsbcoding.com
+  Text Domain:  featured-professor
+  Domain Path: /languages
 */
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -38,6 +40,7 @@ class FeaturedProfessor {
   }
 
   function onInit() {
+    load_plugin_textdomain('featured-professor', false, dirname(plugin_basename(__FILE__)) . '/languages');
     register_meta('post', 'featured_professor', array(
       'show_in_rest' => true,
       'type' => 'number',
@@ -45,6 +48,8 @@ class FeaturedProfessor {
     ));
     wp_register_script('featuredProfessorScript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-i18n', 'wp-editor'));
     wp_register_style('featuredProfessorStyle', plugin_dir_url(__FILE__) . 'build/index.css');
+
+    wp_set_script_translations('featuredProfessorScript', 'featured-professor', plugin_dir_path(__FILE__) . '/languages');
 
     register_block_type('featuredprofplugin/featured-professor', array(
       'render_callback' => [$this, 'renderCallback'],
