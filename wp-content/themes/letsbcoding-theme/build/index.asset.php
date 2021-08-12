312384874160b64ef5f645d41d29f74331b818c7
Maria Campbell <interglobalmedia@gmail.com>
2021-08-12 00:09:05 -0400

    Add new build

--- wp-content/themes/letsbcoding-theme/build/index.asset.php
+++ wp-content/themes/letsbcoding-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '8b54658565c374e3b339f3cec465ece2');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '3b6a5ffe4b22528a54a59b91f81ce1bb');
