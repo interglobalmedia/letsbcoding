d2e74f53f1cc2b18fd9511f7fac3dab79fdaaa2d
Maria Campbell <interglobalmedia@gmail.com>
2021-09-21 18:01:01 -0400

    Add students relationship to pgrograms

--- wp-content/themes/letsbcoding-theme/single-program.php
+++ wp-content/themes/letsbcoding-theme/single-program.php
@@ -16,7 +16,41 @@ while (have_posts()) {
       <?php the_content(); ?>
     </div>
 
-    <?php
+    <?php 
+       $relatedStudents = new WP_Query(array(
+          'posts_per_page' => -1,
+          'post_type' => 'student',
+          'orderby' => 'title',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'related_programs',
+              'compare' => 'LIKE',
+              'value' => '"' . get_the_ID() . '"'
+            ),
+          )
+       ));
+    
+    if ($relatedStudents->have_posts()) {
+      echo '<hr class="section-break">';
+
+      echo '<h2 class="headline headline--medium">' . get_the_title() . ' Students</h2>';
+
+      echo '<ul class="professor-cards">';
+
+      while ($relatedStudents->have_posts()) {
+        $relatedStudents->the_post(); ?>
+        <li class="professor-card__list-item">
+          <a class="professor-card" href="<?php the_permalink(); ?>">
+            <img class="professor-card__image" src="<?php the_post_thumbnail_url('studentLandscape'); ?>">
+            <span class="professor-card__name"><?php the_title(); ?></span>
+          </a>
+        </li>
+      <?php }
+      echo '</ul>';
+    }
+
+    // wp_reset_postdata();
 
     $relatedProfessors = new WP_Query(array(
       'posts_per_page' => -1,
