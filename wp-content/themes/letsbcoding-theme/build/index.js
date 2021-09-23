43b436142bd7e0616fbc27830062552e85019a95
Maria Campbell <interglobalmedia@gmail.com>
2021-09-22 22:40:05 -0400

    Add new highlight nav build

--- wp-content/themes/letsbcoding-theme/build/index.js
+++ wp-content/themes/letsbcoding-theme/build/index.js
@@ -6013,15 +6013,7 @@ class HighlightLink {
 
   linkHandler() {
     for (var i = 0; i < this.links.length; i++) {
-      if (this.links[i].href === document.URL) {
-        this.links[i].classList.add('active');
-      }
-
-      if (this.links[0].href === document.URL || document.URL === `${bcodingData.root_url}`) {
-        this.links[0].classList.add('active');
-      }
-
-      if (this.links[1].href === document.URL || document.URL.includes(`${bcodingData.root_url}/members`)) {
+      if (this.links[1].href == document.URL || document.URL.includes(`${bcodingData.root_url}/members`)) {
         this.links[1].classList.add('active');
       }
     }
