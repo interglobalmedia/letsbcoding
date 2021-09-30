97c0cf9740b4652595a6c5f814638bba53d1de38
Maria Campbell <interglobalmedia@gmail.com>
2021-09-29 20:40:44 -0400

    Add new CommunityHeader class

---
+++ wp-content/themes/letsbcoding-theme/src/modules/CommunityHeader.js
@@ -0,0 +1,28 @@
+class CommunityHeader {
+    constructor() {
+        this.mainNavigationLinks = document.querySelectorAll('a.menu-link')
+        this.communityLinks = document.querySelectorAll('div.menu-link')
+        this.communityHandler()
+    }
+
+    communityHandler() {
+        for (let i = 0; i < this.mainNavigationLinks.length; i++) {
+            if (this.communityLinks[0].href == document.URL || document.URL.includes(`${bcodingData.root_url}/members`)) {
+                this.mainNavigationLinks[0].classList.add('no-nav-link')
+                this.mainNavigationLinks[3].classList.add('no-nav-link')
+                this.mainNavigationLinks[4].classList.add('no-nav-link')
+                this.mainNavigationLinks[5].classList.add('no-nav-link')
+                this.mainNavigationLinks[6].classList.add('no-nav-link')
+            }
+            if (this.communityLinks[0].href == document.URL || document.URL.includes(`${bcodingData.root_url}/groups`)) {
+                this.mainNavigationLinks[0].classList.add('no-nav-link')
+                this.mainNavigationLinks[3].classList.add('no-nav-link')
+                this.mainNavigationLinks[4].classList.add('no-nav-link')
+                this.mainNavigationLinks[5].classList.add('no-nav-link')
+                this.mainNavigationLinks[6].classList.add('no-nav-link')
+            }
+        }
+    }
+}
+
+export default CommunityHeader
