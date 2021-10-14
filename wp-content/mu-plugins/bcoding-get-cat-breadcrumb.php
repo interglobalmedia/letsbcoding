4cfe8b99cecd03fc02b50f541f20208240a897fb
Maria Campbell <interglobalmedia@gmail.com>
2021-10-14 08:26:30 -0400

    Tweak condition to else echo ""

--- wp-content/mu-plugins/bcoding-get-cat-breadcrumb.php
+++ wp-content/mu-plugins/bcoding-get-cat-breadcrumb.php
@@ -1,14 +1,14 @@
 <?php 
 /* Create Breadcrumb functionality for categories */
 function bcoding_get_cat_breadcrumb() {
-    echo '<a href="'.site_url('/categories').'" rel="nofollow">Categories</a>';
     if (is_category() || is_single()) {
+        echo '<a href="'.site_url('/categories').'" rel="nofollow">Categories</a>';
         echo "&nbsp;&nbsp;&#187;&nbsp;&nbsp;";
         the_category(' &bull; ');
-            if (is_single()) {
-                echo " &nbsp;&nbsp;&#187;&nbsp;&nbsp; ";
-                the_title();
-            }
+        if (is_single()) {
+            echo " &nbsp;&nbsp;&#187;&nbsp;&nbsp; ";
+            the_title();
+        }
     } elseif (is_page()) {
         echo "&nbsp;&nbsp;&#187;&nbsp;&nbsp;";
         echo the_title();
@@ -17,6 +17,9 @@ function bcoding_get_cat_breadcrumb() {
         echo '"<em>';
         echo the_search_query();
         echo '</em>"';
+    }
+    else {
+        echo "";
     }
 }
 
