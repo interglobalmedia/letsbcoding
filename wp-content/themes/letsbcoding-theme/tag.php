0dba44575203ce7aa8ec98d9a45bd0c10015b95e
Maria Campbell <interglobalmedia@gmail.com>
2021-10-14 08:34:55 -0400

    remove tag list

--- wp-content/themes/letsbcoding-theme/tag.php
+++ wp-content/themes/letsbcoding-theme/tag.php
@@ -14,7 +14,7 @@
             the_post(); ?>
             <div class="post-item">
                 <h2><a class="headline headline--medium headline--post-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <div class="metabox__tags">
+                <div class="metabox">
                     <p><?php $u_time = get_the_time('U');
 						$u_modified_time = get_the_modified_time('U');
 						if ($u_modified_time >= $u_time + 86400) {
@@ -22,17 +22,13 @@
                             echo " by ";
 							the_author_posts_link();
                             echo " on ";
-							the_modified_time('n.j.y');
-							echo " in ";
-							echo get_the_tag_list('',', ',''); 
+							the_modified_time('n.j.y'); 
 						}
 						else {
 							echo "Posted by ";  
 							the_author_posts_link();
 							echo " on ";
 							the_time('n.j.y');
-							echo " in ";
-							echo get_the_tag_list('',', ','');
 						} 
 					?></p>
                 </div>
