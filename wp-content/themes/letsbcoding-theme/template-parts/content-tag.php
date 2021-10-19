62a7d76eb7f57e02f054609e6e7c91665aaa8932
Maria Campbell <interglobalmedia@gmail.com>
2021-10-19 18:41:05 -0400

    Add esc_url()

--- wp-content/themes/letsbcoding-theme/template-parts/content-tag.php
+++ wp-content/themes/letsbcoding-theme/template-parts/content-tag.php
@@ -1,18 +1,18 @@
 <div class="post-item">
-    <h2><a class="headline headline--medium headline--post-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2><a class="headline headline--medium headline--post-title" href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h2>
     <div class="metabox">
         <p><?php $u_time = get_the_time('U');
             $u_modified_time = get_the_modified_time('U');
             if ($u_modified_time >= $u_time + 86400) {
                 echo "Last updated ";
                 echo " by ";
-                the_author_posts_link();
+                esc_url(the_author_posts_link());
                 echo " on ";
                 the_modified_time('n.j.y'); 
             }
             else {
                 echo "Posted by ";  
-                the_author_posts_link();
+                esc_url(the_author_posts_link());
                 echo " on ";
                 the_time('n.j.y');
             } 
@@ -21,7 +21,7 @@
 
     <div class="generic-content">
         <?php the_excerpt(); ?>
-        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+        <p><a class="btn btn--blue" href="<?php esc_url(the_permalink()); ?>">Continue reading &raquo;</a></p>
     </div>
 
 </div>
