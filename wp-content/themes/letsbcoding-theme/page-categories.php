75b8d060ba10152a36e266957429015383118885
Maria Campbell <interglobalmedia@gmail.com>
2021-10-19 11:42:54 -0400

    Add template part

--- wp-content/themes/letsbcoding-theme/page-categories.php
+++ wp-content/themes/letsbcoding-theme/page-categories.php
@@ -10,31 +10,7 @@
         <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog');  ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a>
     </div>
     <?php 
-        $categories = get_categories(array(
-            'child_of'            => 0,
-            'current_category'    => 0,
-            'depth'               => 0,
-            'echo'                => 0,
-            'exclude'             => '',
-            'exclude_tree'        => '',
-            'feed'                => '',
-            'feed_image'          => '',
-            'feed_type'           => '',
-            'hide_empty'          => 0,
-            'hide_title_if_empty' => false,
-            'hierarchical'        => true,
-            'order'               => '',
-            'orderby'             => 'hierarchical',
-            'separator'           => '<br />',
-            'show_count'          => 0,
-            'show_option_all'     => '',
-            'show_option_none'    => __( 'No categories' ),
-            'style'               => 'list',
-            'taxonomy'            => 'category',
-            'title_li'            => __( 'Categories' ),
-            'use_desc_for_title'  => 1,
-        ));
-        wp_list_categories($args);
+      get_template_part('template-parts/content', 'categories');
     ?>
 </div>
 
