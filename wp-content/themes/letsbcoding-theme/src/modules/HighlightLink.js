586f3d63948fd9c32e07c7fbdd00ce011b80f32e
Maria Campbell <interglobalmedia@gmail.com>
2021-10-21 13:24:58 -0400

    Tweak nav highlighting re new Curriculum sub-nav

--- wp-content/themes/letsbcoding-theme/src/modules/HighlightLink.js
+++ wp-content/themes/letsbcoding-theme/src/modules/HighlightLink.js
@@ -1,18 +1,21 @@
 class HighlightLink {
     constructor() {
         this.links = document.querySelectorAll('.menu-link')
-        this.subLinks = document.querySelectorAll('ul.sub-nav li a')
+        this.subCommunityLinks = document.querySelectorAll('ul.sub-nav-community li a')
+        this.subCurriculumLinks = document.querySelectorAll('ul.sub-nav-curriculum li a')
+        this.curriculumLink = document.querySelector(".menu-link.nav-highlight-curriculum")
         this.logoText = document.querySelector('.site-header h1 a')
-        this.linkHandler()
+        this.communityLinkHandler()
+        this.curriculumLinkHandler()
     }
 
-    linkHandler() {
+    communityLinkHandler() {
         for (let i = 0; i < this.links.length; i++) {
             if (this.links[1].href == document.URL || document.URL.includes(`${bcodingData.root_url}/members`)) {
                 this.links[1].classList.add('active')
             }
             if (this.links[1].href == document.URL || document.URL.includes(`${bcodingData.root_url}/members`)) {
-                this.subLinks[0].classList.add('active')
+                this.subCommunityLinks[0].classList.add('active')
             }
             if (this.links[3].href == document.URL || document.URL.includes(`${bcodingData.root_url}/events`)) {
                 this.links[3].classList.add('current-menu-item')
@@ -21,13 +24,26 @@ class HighlightLink {
                 this.links[1].classList.add('active')
             }
             if (this.links[1].href == document.URL || document.URL.includes(`/group`) || document.URL.includes(`/groups`)) {
-                this.subLinks[1].classList.add('active')
+                this.subCommunityLinks[1].classList.add('active')
             }
             if (this.links[i].href == document.URL || document.URL.includes(`/student`) || document.URL.includes(`/professor`)) {
                 this.links[4].classList.add('active')
             }
             if (this.logoText.href == document.URL) {
                 this.logoText.classList.add('active')
+            }
+        }
+    }
+
+    curriculumLinkHandler() {
+        for (let i = 0; i < this.links.length; i++) {
+            if (this.links[2].href == document.URL || document.URL.includes(`${bcodingData.root_url}/programs`)) {
+                this.curriculumLink.classList.add('active')
+                this.subCurriculumLinks[0].classList.add('active')
+            }
+            if (this.links[2].href == document.URL || document.URL.includes(`${bcodingData.root_url}/courses`)) {
+                this.curriculumLink.classList.add('active')
+                this.subCurriculumLinks[1].classList.add('active')
             }
         }
     }
