b6cc2c80c14cd048c447f7b0ed21051869da195d
Maria Campbell <interglobalmedia@gmail.com>
2021-12-15 11:53:19 -0500

    LET-22 #resolved #comment bring file up to WP Code Standards PHPCS

--- wp-content/themes/letsbcoding-theme/archive-campus.php
+++ wp-content/themes/letsbcoding-theme/archive-campus.php
@@ -1,27 +1,44 @@
 <?php
+/**
+ * Template for campuses content in the LetsBCoding Theme.
+ *
+ * @author Maria D. Campbell interglobalmedia@interglobalmedianetwork.com
+ * @copyright Copyright (c) 2021 Maria D. Campbell
+ *
+ * @package WordPress
+ * @subpackage LetsBCoding
+ * @link https://www.interglobalmedianetwork.com
+ * @since LetsBCoding 2.0.0
+ */
+
+?>
+
+<?php
 
 get_header();
-pageBanner(array(
-  'title' => 'Our Campuses',
-  'subtitle' => 'We have several conveniently located campuses.'
-));
+page_banner(
+	array(
+		'title'    => 'Our Campuses',
+		'subtitle' => 'We have several conveniently located campuses.',
+	)
+);
 ?>
 
 <div class="container container--narrow page-section">
 
-  <div class="acf-map">
+	<div class="acf-map">
 
-    <?php
-    while (have_posts()) {
-      the_post();
-      $map_location = get_field('map_location');
-    ?>
-      <div class="marker" data-lat="<?php echo $map_location['lat'] ?>" data-lng="<?php echo $map_location['lng']; ?>">
-        <h3><a href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h3>
-        <?php echo $map_location['address']; ?>
-      </div>
-    <?php } ?>
-  </div>
+	<?php
+	while ( have_posts() ) {
+		the_post();
+		$map_location = get_field( 'map_location' );
+		?>
+		<div class="marker" data-lat="<?php echo esc_attr( $map_location['lat'] ); ?>" data-lng="<?php echo esc_attr( $map_location['lng'] ); ?>">
+		<h3><a href="<?php echo esc_url( get_the_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a></h3>
+		<?php echo esc_html( $map_location['address'] ); ?>
+		</div>
+	<?php } ?>
+	</div>
 
 </div>
 
