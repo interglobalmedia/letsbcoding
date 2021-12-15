5aa6803f01e94e60a77c98ccdb36c60134872b4b
Maria Campbell <interglobalmedia@gmail.com>
2021-12-15 13:11:16 -0500

    LET-57 #resolved #comment bring file up to WP Code Standards PHPCS

--- wp-content/themes/letsbcoding-theme/page-professors.php
+++ wp-content/themes/letsbcoding-theme/page-professors.php
@@ -1,8 +1,40 @@
-<?php get_header(); 
-  pageBanner(array(
-    'title' => 'All Professors',
-    'subtitle' => 'A list of professors teaching at LetsBCoding'
-  ));
+<?php
+/**
+ * Professors page template professor custom post type for the LetsBCoding Theme.
+ *
+ * @author Maria D. Campbell interglobalmedia@interglobalmedianetwork.com
+ * @copyright Copyright (c) 2021 Maria D. Campbell
+ *
+ * @package WordPress
+ * @subpackage LetsBCoding
+ * @link https://www.interglobalmedianetwork.com
+ * @since LetsBCoding 2.0.0
+ */
 
-get_footer();
 ?>
+
+<?php
+get_header();
+page_banner(
+	array(
+		'title'    => 'Professors',
+		'subtitle' => 'Professors teaching at LetsBCoding',
+	)
+);
+?>
+
+<div class="container container--narrow page-section">
+<div class="metabox metabox--position-up metabox--with-home-link">
+	<p><a class="metabox__blog-home-link" 
+href="<?php echo esc_url( site_url( '/programs' ) ); ?>"><i class="fa fa-home" aria-hidden="true"></i> Programs Home</a></p>
+	</div>
+<?php
+while ( have_posts() ) {
+	the_post();
+	get_template_part( 'template-parts/content', 'professors' );
+}
+	echo wp_kses_post( paginate_links() );
+?>
+</div>
+
+<?php get_footer(); ?>
