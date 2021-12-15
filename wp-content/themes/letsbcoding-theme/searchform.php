157707d5de58787f28af2230df9700c22fd86f28
Maria Campbell <interglobalmedia@gmail.com>
2021-12-15 13:17:50 -0500

    LET-62 #resolved #comment bring file up to WP Code Standards PHPCS

--- wp-content/themes/letsbcoding-theme/searchform.php
+++ wp-content/themes/letsbcoding-theme/searchform.php
@@ -1,7 +1,22 @@
-<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-    <label class="headline headline--medium" for="s">Perform a New Search:</label>
-    <div class="search-form-row">
-        <input class="s" id="s" type="search" name="s" placeholder="Search for ...">
-        <button class="search-submit" type="submit">Submit</button>
-    </div>
+<?php
+/**
+ * Default WordPress Search page search form template for the LetsBCoding Theme.
+ *
+ * @author Maria D. Campbell interglobalmedia@interglobalmedianetwork.com
+ * @copyright Copyright (c) 2021 Maria D. Campbell
+ *
+ * @package WordPress
+ * @subpackage LetsBCoding
+ * @link https://www.interglobalmedianetwork.com
+ * @since LetsBCoding 2.0.0
+ */
+
+?>
+
+<form class="search-form" method="get" action="<?php echo esc_url( site_url( '/' ) ); ?>">
+	<label class="headline headline--medium" for="s">Perform a New Search:</label>
+	<div class="search-form-row">
+		<input class="s" id="s" type="search" name="s" placeholder="Search for ...">
+		<button class="search-submit" type="submit">Submit</button>
+	</div>
 </form>
