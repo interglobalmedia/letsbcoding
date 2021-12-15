b68b82ce5b845a75b1e2fce8c7b9d2022cf2b4f2
Maria Campbell <interglobalmedia@gmail.com>
2021-12-15 13:15:07 -0500

    LEt-60 #resolved #commment bring up file to WP Code Standards PHPCS

--- wp-content/themes/letsbcoding-theme/page.php
+++ wp-content/themes/letsbcoding-theme/page.php
@@ -1,58 +1,65 @@
 <?php
+/**
+ * Page template for the LetsBCoding Theme.
+ *
+ * @author Maria D. Campbell interglobalmedia@interglobalmedianetwork.com
+ * @copyright Copyright (c) 2021 Maria D. Campbell
+ *
+ * @package WordPress
+ * @subpackage LetsBCoding
+ * @link https://www.interglobalmedianetwork.com
+ * @since LetsBCoding 2.0.0
+ */
+
+?>
+
+<?php
 
 get_header();
-while(have_posts()) {
-	the_post(); 
-	pageBanner();
+while ( have_posts() ) {
+	the_post();
+	page_banner();
 	?>
 
-  <div class="container container--narrow page-section">
-	<?php 
-	$theParent = wp_get_post_parent_id(get_the_ID());
-	if ($theParent) { ?>
+	<div class="container container--narrow page-section">
+			<?php
+			$the_parent = wp_get_post_parent_id( get_the_ID() );
+			$test_array = get_pages(
+				array(
+					'child_of' => get_the_ID(),
+				)
+			);
 
-		<div class="metabox metabox--position-up metabox--with-home-link">
-			<p><a class="metabox__blog-home-link" href="<?php the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php echo the_title(); ?></span></p>
+			if ( $the_parent || $test_array ) {
+				?>
+		<div class="page-links">
+			<h2 class="page-links__title"><a href="<?php echo esc_url( get_the_permalink( $the_parent ) ); ?>"><?php echo esc_html( get_the_title( $the_parent ) ); ?></a></h2>
+			<ul class="min-list">
+				<?php
+
+				if ( $the_parent ) {
+					$find_children_of = $the_parent;
+				} else {
+					$find_children_of = get_the_ID();
+				}
+
+				wp_list_pages(
+					array(
+						'title_li'    => null,
+						'child_of'    => $find_children_of,
+						'sort_column' => 'menu_order',
+					)
+				);
+				?>
+			</ul>
 		</div>
-
-	<?php }
-
-	?>
-    
-	<?php 
-
-	$testArray = get_pages(array(
-		'child_of' => get_the_ID()
-	));
-	
-	if($theParent or $testArray) { ?>
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="<?php the_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
-      <ul class="min-list">
-        <?php 
-
-			if ($theParent) {
-				$findChildrenOf = $theParent;
-			} else {
-				$findChildrenOf = get_the_ID();
-			}
-
-			wp_list_pages(array(
-				'title_li' => NULL,
-				'child_of' => $findChildrenOf,
-				'sort_column' => 'menu_order'
-			));
+		<?php } ?>
+		<div class="generic-content">
+		<?php
+			echo wp_kses_post( apply_filters( 'the_content', get_the_content() ) );
 		?>
-      </ul>
-    </div>
-	<?php } ?>
-
-    <div class="generic-content">
-      <?php the_content();
-	  ?>
-    </div>
-
-  </div>
+		</div>
+	</div>
 
 <?php }
 
