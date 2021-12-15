b33dcf0004a87dc0a5ddced41489b06d79238af1
Maria Campbell <interglobalmedia@gmail.com>
2021-12-15 14:06:24 -0500

    LET-86 #resolved #comment bring file up to WP Code Standards PHPCS

--- wp-content/themes/letsbcoding-theme/template-parts/content-student.php
+++ wp-content/themes/letsbcoding-theme/template-parts/content-student.php
@@ -1,8 +1,24 @@
+<?php
+/**
+ * Template part for displaying student content in the LetsBCoding Theme in the default WordPress search using search.php.
+ *
+ * @author Maria D. Campbell interglobalmedia@interglobalmedianetwork.com
+ * @copyright Copyright (c) 2021 Maria D. Campbell
+ *
+ * @package WordPress
+ * @subpackage LetsBCoding
+ * @link https://www.interglobalmedianetwork.com
+ * @since LetsBCoding 2.0.0
+ */
+
+?>
+
+<h3 class="headline headline--medium">Student</h3>
 <div class="post-item">
-    <li class="professor-card__list-item">
-        <a class="professor-card" href="<?php esc_url(the_permalink()); ?>">
-        <img class="professor-card__image" src="<?php esc_url(the_post_thumbnail_url('studentLandscape')); ?>">
-        <span class="professor-card__name"><?php the_title(); ?></span>
-        </a>
-    </li>
+	<li class="professor-card__list-item">
+		<a class="professor-card" href="<?php echo esc_url( get_the_permalink() ); ?>">
+		<img class="professor-card__image" src="<?php echo esc_url( get_the_post_thumbnail_url( 'professorLandscape' ) ); ?>">
+		<span class="professor-card__name"><?php the_title(); ?></span>
+		</a>
+	</li>
 </div>
