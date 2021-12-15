a7e2da080b610e8e2880bd8d8689320fa950c7d2
Maria Campbell <interglobalmedia@gmail.com>
2021-12-15 14:03:19 -0500

    LET-84 #resolved #comment bring file up to WP Code Standards PHPCS

--- wp-content/themes/letsbcoding-theme/template-parts/content-professor.php
+++ wp-content/themes/letsbcoding-theme/template-parts/content-professor.php
@@ -1,53 +1,24 @@
 <?php
-
-$allProfessors = new WP_Query(array(
-    'posts_per_page' => -1,
-    'post_type' => 'professor',
-    'orderby' => 'title',
-    'order' => 'ASC',
-    'meta_query' => array(
-        array(
-            'key' => 'professor_id',
-            'compare' => '=',
-            'value' => '"' . get_the_ID() . '"'
-        ),
-    )
-));
-
-    $all_professors_hr_class_section_break = '<hr class="section-break">';
-    $all_professors_h3_class_headline_profs = '<h3 class="headline headline--medium headline--all-professors">' . esc_html(get_the_title()) . '</h3>';
-    $all_professors_ul_class_prof_cards = '<ul class="professor-cards professor-cards__all-professors">';
-    $all_professors_ul_closing_tag = '</ul>';
-
-echo wp_kses($all_professors_hr_class_section_break, array('hr' => array('class' => array()
-        )
-    ));
-echo wp_kses($all_professors_h3_class_headline_profs, array('h3' => array(
-            'class' => array()
-        )
-    ));
-
-echo wp_kses($all_professors_ul_class_prof_cards, array('ul' => array(
-            'class' => array()
-        )
-    ));
-
-while ($allProfessors->have_posts()) {
-        $allProfessors->the_post(); ?>
-<!--        <div class="post-item">-->
-            <li class="professor-card__list-item">
-                <a class="professor-card" href="<?php echo esc_url(get_the_permalink()); ?>">
-                    <img class="professor-card__image" src="<?php echo esc_attr(get_the_post_thumbnail_url($the_query->ID, 'professorLandscape')); ?>">
-                    <span class="professor-card__name"><?php echo esc_html(get_the_title()); ?></span>
-                </a>
-            </li>
-            <?php
-            ?>
-<!--        </div>-->
-    <?php }
-
-echo wp_kses($all_professors_ul_closing_tag, array(
-    'ul' => array()
-));
+/**
+ * Template part for displaying professor content in the LetsBCoding Theme in the default WordPress search using search.php.
+ *
+ * @author Maria D. Campbell interglobalmedia@interglobalmedianetwork.com
+ * @copyright Copyright (c) 2021 Maria D. Campbell
+ *
+ * @package WordPress
+ * @subpackage LetsBCoding
+ * @link https://www.interglobalmedianetwork.com
+ * @since LetsBCoding 2.0.0
+ */
 
 ?>
+
+<h3 class="headline headline--medium">Professor</h3>
+<div class="post-item">
+	<li class="professor-card__list-item">
+		<a class="professor-card" href="<?php echo esc_url( get_the_permalink() ); ?>">
+		<img class="professor-card__image" src="<?php echo esc_url( get_the_post_thumbnail_url( 'professorLandscape' ) ); ?>">
+		<span class="professor-card__name"><?php esc_html( get_the_title() ); ?></span>
+		</a>
+	</li>
+</div>
